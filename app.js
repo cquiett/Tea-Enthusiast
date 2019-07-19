4c909c3230aeadaaf4fd22056f3f1af675d37492
cquiett <51850611+cquiett@users.noreply.github.com>
2019-07-19 17:09:22 -0400

    coded app just need to make adjustments

---
+++ app.js
@@ -0,0 +1,78 @@
+$(() => {
+//=====================
+//GRABBING ALL ELEMENTS
+//=====================
+//counter variable to keep track of the current image index
+let currentImgIndex = 0
+//current image element
+let $currentImg = $('.carousel-images').children().eq(currentImgIndex)
+// number of images
+let numOfImages = $('.carousel-images').children().length-1
+//=====================
+//GRABBING ALL ELEMENTS
+//=====================
+//next button
+const $next = $('.next')
+//previous button
+const $previous = $('.previous')
+
+//======================
+//EVENT LISTENER/HANDLER
+//======================
+$next.on('click', () => {
+  // console.log('clicking next');
+//we want the current image to hide
+//grabbing all carousel images
+  // console.log($('.carousel-images').children());
+//then show the next image
+//hiding the current image
+$currentImg.hide()
+//check if the currentImgIndex is less than the amount of images we have
+if(currentImgIndex < numOfImages) {
+  //Increment current image index
+  currentImgIndex++
+} else {//if the currentImgIndex > the amount of images we have reset the currentImgIndex to 0, so we cycle back
+  currentImgIndex = 0
+}
+//change the currentImg
+$currentImg = $('.carousel-images').children().eq(currentImgIndex)
+//show the new currentImg
+$currentImg.show()
+  })
+
+//previous button
+$previous.on('click', () => {
+  console.log('previous button');
+//hide the current image
+$currentImg.hide()
+//check if the currentImgIndex > 0
+if (currentImgIndex > 0) {
+//decrement the current image index
+  currentImgIndex--
+} else {// if the currentImgIndex < 0, reset the currentImgIndex to the numOfImages
+  currentImgIndex = numOfImages
+}
+//change the currentImg
+$currentImg = $('.carousel-images').children().eq(currentImgIndex)
+//show the new currentImg
+$currentImg.show()
+  })
+
+  $('form').on('submit', (event) => {
+          event.preventDefault();
+          const userInput = $('input[type="text"]').val();
+          $.ajax({
+            url:'https://www.googleapis.com/books/v1/volumes?q=tea,
+          }).then(
+            (data) => {
+              // $('#title').html(data.Title);
+              // $('#year').html(data.Year);
+              // $('#rated').html(data.Rated);
+            },
+            (error) => {
+              console.log(error);
+            }
+          );
+
+  });
+})
